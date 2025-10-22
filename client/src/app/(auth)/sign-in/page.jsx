4e6df7360d9402c02d69dc5a0d46b44c167a920d
bilalAn-nur <bilalannurfauzi@gmail.com>
2025-10-22 19:43:49 +0700

    feat: add metadata generation for sign-in page

--- client/src/app/(auth)/sign-in/page.jsx
+++ client/src/app/(auth)/sign-in/page.jsx
@@ -1,5 +1,12 @@
 import AuthForm from "@/components/AuthForm";
 
+export async function generateMetadata({ params, searchParams }) {
+  return {
+    title: "Sign-in",
+    description: "Halaman Sign-in",
+  };
+}
+
 export default function LoginPage() {
   return <AuthForm type="login" />;
 }
