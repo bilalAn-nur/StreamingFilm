2d8679e60ff6bce5c9e9fcdd783c106af1bcd316
bilalAn-nur <bilalannurfauzi@gmail.com>
2025-10-23 15:46:33 +0700

    feat: restore layout and page components with updated structure and styles

--- client/src/components/home/Sidebar.jsx
+++ client/src/components/home/Sidebar.jsx
@@ -4,85 +4,55 @@ const Sidebar = () => {
   return (
     <div className="bg-gray-900 text-white w-20 flex flex-col items-center py-6 space-y-6 fixed h-screen">
       <h1 className="text-red-600 font-bold text-xl">INDI</h1>
-      <div className="space-y-6 mt-10">
+      <div className="space-y-12 mt-10">
         {/* Home Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer hover:text-red-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 640 640"
+          className="h-6 w-6 cursor-pointer text-white hover:text-red-500 transition-colors"
+          fill="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3 12l2-2m0 0l7-7 7 7M13 5v6h6m-6 0L5 10v10h14V10l-7-7z"
-          />
+          <path d="M341.8 72.6C329.5 61.2 310.5 61.2 298.3 72.6L74.3 280.6C64.7 289.6 61.5 303.5 66.3 315.7C71.1 327.9 82.8 336 96 336L112 336L112 512C112 547.3 140.7 576 176 576L464 576C499.3 576 528 547.3 528 512L528 336L544 336C557.2 336 569 327.9 573.8 315.7C578.6 303.5 575.4 289.5 565.8 280.6L341.8 72.6zM304 384L336 384C362.5 384 384 405.5 384 432L384 528L256 528L256 432C256 405.5 277.5 384 304 384z" />
         </svg>
 
         {/* Search Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer hover:text-red-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 640 640"
+          className="h-6 w-6 cursor-pointer text-white hover:text-red-500 transition-colors"
+          fill="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 16a8 8 0 1116 0 8 8 0 01-16 0zm8-8a8 8 0 10-8 8 8 8 0 008-8zm-3 7l4 4"
-          />
+          <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" />
         </svg>
 
         {/* Collection Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer hover:text-red-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 640 640"
+          className="h-6 w-6 cursor-pointer text-white hover:text-red-500 transition-colors"
+          fill="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3 7h18M3 12h18M3 17h18"
-          />
+          <path d="M96 160C96 124.7 124.7 96 160 96L480 96C515.3 96 544 124.7 544 160L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 160zM144 432L144 464C144 472.8 151.2 480 160 480L192 480C200.8 480 208 472.8 208 464L208 432C208 423.2 200.8 416 192 416L160 416C151.2 416 144 423.2 144 432zM448 416C439.2 416 432 423.2 432 432L432 464C432 472.8 439.2 480 448 480L480 480C488.8 480 496 472.8 496 464L496 432C496 423.2 488.8 416 480 416L448 416zM144 304L144 336C144 344.8 151.2 352 160 352L192 352C200.8 352 208 344.8 208 336L208 304C208 295.2 200.8 288 192 288L160 288C151.2 288 144 295.2 144 304zM448 288C439.2 288 432 295.2 432 304L432 336C432 344.8 439.2 352 448 352L480 352C488.8 352 496 344.8 496 336L496 304C496 295.2 488.8 288 480 288L448 288zM144 176L144 208C144 216.8 151.2 224 160 224L192 224C200.8 224 208 216.8 208 208L208 176C208 167.2 200.8 160 192 160L160 160C151.2 160 144 167.2 144 176zM448 160C439.2 160 432 167.2 432 176L432 208C432 216.8 439.2 224 448 224L480 224C488.8 224 496 216.8 496 208L496 176C496 167.2 488.8 160 480 160L448 160z" />
         </svg>
 
         {/* Star Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer hover:text-red-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 640 640"
+          className="h-6 w-6 cursor-pointer text-white hover:text-red-500 transition-colors"
+          fill="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l2.15 6.607a1 1 0 00.95.69h6.955c.969 0 1.371 1.24.588 1.81l-5.64 4.107a1 1 0 00-.364 1.118l2.15 6.607c.3.921-.755 1.688-1.54 1.118l-5.64-4.107a1 1 0 00-1.175 0l-5.64 4.107c-.785.57-1.84-.197-1.54-1.118l2.15-6.607a1 1 0 00-.364-1.118L2.406 11.044c-.783-.57-.38-1.81.588-1.81h6.955a1 1 0 00.95-.69l2.15-6.607z"
-          />
+          <path d="M192 64C156.7 64 128 92.7 128 128L128 544C128 555.5 134.2 566.2 144.2 571.8C154.2 577.4 166.5 577.3 176.4 571.4L320 485.3L463.5 571.4C473.4 577.3 485.7 577.5 495.7 571.8C505.7 566.1 512 555.5 512 544L512 128C512 92.7 483.3 64 448 64L192 64z" />
         </svg>
 
         {/* User Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer hover:text-red-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 640 640"
+          className="h-6 w-6 cursor-pointer text-white hover:text-red-500 transition-colors"
+          fill="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5.121 17.804A8.001 8.001 0 1112 20a8.001 8.001 0 01-6.879-2.196zM15 11a3 3 0 11-6 0 3 3 0 016 0z"
-          />
+          <path d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" />
         </svg>
       </div>
     </div>
